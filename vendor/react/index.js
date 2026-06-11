c6e9bf9e76ccfa153b81922da6db5b99f9defccd
pina753753-star <pina753753@gmail.com>
2026-06-12 06:07:23 +0900

    Remove old Vite scaffold vendor files

--- vendor/react/index.js
+++
@@ -1,7 +0,0 @@
-export const StrictMode = ({ children }) => children;
-
-export function createElement(type, props, ...children) {
-  return { type, props: props ?? {}, children: children.flat(Infinity) };
-}
-
-export default { StrictMode, createElement };
